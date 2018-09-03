37e4f221c73e78f991e856fa7eda711f98becae4
djhdesign <abcorey7@gmail.com>
2018-09-03 01:23:48 -0400

    Changing how secrets should work

---
+++ server/src/helpers/secret.ts
@@ -0,0 +1,10 @@
+if (!process.env.NODE_ENV) {
+  require('dotenv').load();
+}
+
+export default {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
+};
